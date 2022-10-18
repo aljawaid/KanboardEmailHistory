9d0504d5eac0776c08f297d292877b091ee80451
aljawaid <jawaid_imran@hotmail.com>
2022-10-18 19:31:33 +0100

    NEW: Added `en_GB` translations

--- Locale/en_GB/translations.php
+++ Locale/en_GB/translations.php
@@ -1,1 +1,9 @@
-
+<?php
+return array(
+  'Send full task description and comments by email' => 'Send full task description and comments by email',
+  'Email subject' => 'Email Subject',
+  'Send to Assignee' => 'Send to Assignee',
+  'Send to Creator' => 'Send to Creator',
+  'Send to Both' => 'Send to Both',
+  'Include Task Title and ID in subject line?' => 'Include Task Title and ID in the subject line?',
+);
