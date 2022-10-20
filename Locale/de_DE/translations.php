c143eca8db8cac8c82bf22c362395fe85df17fe9
aljawaid <jawaid_imran@hotmail.com>
2022-10-20 19:47:57 +0100

    NEW: Add Translations
    - Added French
    - Added German
    - Used Google Translate for translations

---
+++ Locale/de_DE/translations.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+  'Send full task description and comments by email' => 'Senden Sie die vollständige Aufgabenbeschreibung und Kommentare per E-Mail',
+  'Email subject' => 'E-Mail Betreff',
+  'Send to Assignee' => 'An Zuständigen senden',
+  'Send to Creator' => 'An Ersteller senden',
+  'Send to Both' => 'An beide senden',
+  'Include Task Title and ID in subject line?' => 'Titel und ID der Aufgabe in die Betreffzeile aufnehmen?',
+  'Task history emailed to the task creator "%s" with subject "%s".' => 'Der Aufgabenverlauf wurde per E-Mail an @"%s" (Aufgabenersteller) mit dem Betreff "%s" gesendet.',
+  'Task history emailed to the task assignee "%s" with subject "%s".' => 'Der Aufgabenverlauf wurde per E-Mail an @"%s" (Aufgabenverantwortlicher) mit dem Betreff "%s" gesendet.',
+);
---
+++ Locale/fr_FR/translations.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+  'Send full task description and comments by email' => 'Envoyer la description complète de la tâche et les commentaires par e-mail',
+  'Email subject' => 'Sujet du Courriel',
+  'Send to Assignee' => 'Envoyer au Cessionnaire',
+  'Send to Creator' => 'Envoyer au Créateur',
+  'Send to Both' => 'Envoyer aux Deux',
+  'Include Task Title and ID in subject line?' => 'Inclure le titre et l\'ID de la tâche dans la ligne d\'objet?',
+  'Task history emailed to the task creator "%s" with subject "%s".' => 'L\'historique des tâches a été envoyé par e-mail à @"%s" (Créateur de tâches) avec pour objet "%s".',
+  'Task history emailed to the task assignee "%s" with subject "%s".' => 'L\'historique des tâches a été envoyé par e-mail à @"%s" (Responsable de la tâche) avec le sujet "%s".',
+);
