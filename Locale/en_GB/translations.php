4ace07226681fe8f83aea97bb3a296d5c5389b1d
aljawaid <jawaid_imran@hotmail.com>
2022-10-20 16:39:12 +0100

    UPDATE: Translations
    - Split language strings for creator and assignee

--- Locale/en_GB/translations.php
+++ Locale/en_GB/translations.php
@@ -6,5 +6,6 @@ return array(
   'Send to Creator' => 'Send to Creator',
   'Send to Both' => 'Send to Both',
   'Include Task Title and ID in subject line?' => 'Include Task Title and ID in the subject line?',
-  'Task history emailed to "%s" with subject "%s".' => 'Task history emailed to "%s" with subject "%s".',
+  'Task history emailed to the task creator "%s" with subject "%s".' => 'Task history has been emailed to @"%s" (Task Creator) with the subject "%s".',
+  'Task history emailed to the task assignee "%s" with subject "%s".' => 'Task history has been emailed to @"%s" (Task Assignee) with the subject "%s".',
 );
