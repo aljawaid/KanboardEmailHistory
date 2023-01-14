009d1ff9b65dbd7b740be79c087b9b6251c7dffc
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-14 17:14:53 +0000

    Code Cleanup

--- Plugin.php
+++ Plugin.php
@@ -10,10 +10,8 @@ class Plugin extends Base
 
 {
     
-	public function initialize()
-    
+    public function initialize()
 	{
-        
 		$this->actionManager->register(new EmailTaskHistory($this->container));
 		
 		if (!file_exists('plugins/AutoSubtasks')) {
@@ -23,14 +21,19 @@ class Plugin extends Base
 	}
 	
 	public function onStartup()
-        {
-               Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
-        }
+    {
+      	Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
+    }
 
-	
 	public function getPluginName()	
 	{ 		 
-		return 'KanboardEmailHistory'; 
+		// Plugin Name MUST be identical to namespace for Plugin Directory to detect updated versions
+		return 'KanboardEmailHistory';
+	}
+
+	public function getPluginDescription()
+	{
+		return t('This plugin adds a new Automatic Action to provide the user with a final email report. Upon closing tasks, automatic emails can be sent of each task description with full comment history to selected recipients or the assigned project email address.');
 	}
 
 	public function getPluginAuthor() 
@@ -43,22 +46,18 @@ class Plugin extends Base
 		return '1.2.0'; 
 	}
 
-    	public function getCompatibleVersion()
-    	{
-        	// Examples:
-        	// >=1.0.37
-        	// <1.0.37
-        	// <=1.0.37
-        	return '>=1.2.20';
-    	}	
-
-	public function getPluginDescription() 
-	{ 
-		return t('This plugin adds a new Automatic Action to provide the user with a final email report. Upon closing tasks, automatic emails can be sent of each task description with full comment history to selected recipients or the assigned project email address.');
-	}
+    public function getCompatibleVersion()
+   	{
+       	// Examples:
+       	// >=1.0.37
+        // <1.0.37
+       	// <=1.0.37
+        return '>=1.2.20';
+    }
 
 	public function getPluginHomepage() 
 	{ 	 
 		return 'https://github.com/aljawaid/KanboardEmailHistory'; 
 	}
+
 }
