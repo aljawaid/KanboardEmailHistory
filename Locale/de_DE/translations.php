20baacb2dcc8121a777ef0c30a78fac55505a572
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-14 10:11:37 +0000

    Reset `de_DE` and `fr_FR` translations
    - #9

--- Locale/de_DE/translations.php
+++ Locale/de_DE/translations.php
@@ -1,13 +1,80 @@
 <?php
 return array(
-  'This action plugin adds a new action to the Kanboard user workflow process. Upon closing tasks, automatically email the task description and full history of comments to selected recipients.' => 'Dieses Aktions-Plug-in fügt dem Kanboard-Benutzer-Workflow-Prozess eine neue Aktion hinzu. Senden Sie beim Schließen von Aufgaben automatisch die Aufgabenbeschreibung und den vollständigen Verlauf der Kommentare per E-Mail an ausgewählte Empfänger.',
-  //'Send full task description and comments by email' => 'Senden Sie die vollständige Aufgabenbeschreibung und Kommentare per E-Mail',
-  'EmailTaskHistory > Send task description and complete comment history by email on task closure' => 'EmailTaskHistory > Aufgabenbeschreibung und vollständigen Kommentarverlauf per E-Mail beim Aufgabenabschluss senden',
-  'Email subject' => 'E-Mail Betreff',
-  'Send to Assignee' => 'An Zuständigen senden',
-  'Send to Creator' => 'An Ersteller senden',
-  'Send to Both' => 'An beide senden',
-  'Include Task Title and ID in subject line?' => 'Titel und ID der Aufgabe in die Betreffzeile aufnehmen?',
-  'Task history emailed to the task creator @"%s" with subject "%s".' => 'Der Aufgabenverlauf wurde per E-Mail an @"%s" (Aufgabenersteller) mit dem Betreff "%s" gesendet.',
-  'Task history emailed to the task assignee @"%s" with subject "%s".' => 'Der Aufgabenverlauf wurde per E-Mail an @"%s" (Aufgabenverantwortlicher) mit dem Betreff "%s" gesendet.',
+  //
+  // GENERAL
+  //
+  'This plugin adds a new Automatic Action to provide the user with a final email report. Upon closing tasks, automatic emails can be sent of each task description with full comment history to selected recipients or the assigned project email address.' => '',
+  'Email subject' => '',
+  //
+  // Action/EmailTaskHistory.php
+  //
+  'EmailTaskHistory > Send task description and complete comment history on task closure' => '',
+  'Send to Assignee' => '',
+  'Send to Creator' => '',
+  'Assignee & Creator' => '',
+  'Assignee & Project Email' => '',
+  'Creator & Project Email' => '',
+  'Project Email' => '',
+  'Assignee, Creator & Project Email' => '',
+  'Include the Task Title and ID in subject line?' => '',
+  'Include the Project Name in the subject line?' => '',
+  'Include the Project Identifier in the subject line?' => '',
+  'Task history emailed to the task assignee @"%s" with subject "%s".' => '',
+  'Task history emailed to the task creator @"%s" with subject "%s".' => '',
+  'Task history emailed to the project email address with subject "%s".' => '',
+  //
+  // action_creation/params.php
+  //
+  'Define action parameters' => '',
+  'Action' => '',
+  'Event' => '',
+  'Options' => '',
+  //
+  // comment/show.php
+  //
+  'Created at:' => '',
+  'Local Time' => '',
+  'LT' => '',
+  'Updated at:' => '',
+  'ID: ' => '',
+  //
+  // notification/footer.php
+  //
+  'System Generated Email' => '',
+  'View Task' => '',
+  'View Board' => '',
+  //
+  // notification/task_create.php
+  //
+  'Summary' => '',
+  'Task Status:' => '',
+  'Task N°:' => '',
+  'Project:' => '',
+  'Priority:' => '',
+  'Reference:' => '',
+  'Complexity:' => '',
+  'Created:' => '',
+  'Started:' => '',
+  'Modified:' => '',
+  'Due Date:' => '',
+  'Completed:' => '',
+  'Created by %s' => '',
+  'Assigned to %s' => '',
+  'Nobody was assigned to this task' => '',
+  'Board Column:' => '',
+  'Swimlane:' => '',
+  'Category:' => '',
+  'Task Position:' => '',
+  'Recurrence:' => '',
+  'Recurrent task is scheduled to be generated' => '',
+  'Recurrent task has been generated' => '',
+  'This task was created by: ' => '',
+  'This task has created this child task: ' => '',
+  'Description' => '',
+  //
+  // task_comments/show.php
+  //
+  'Comments' => '',
+  'No comments' => '',
 );
+
--- Locale/fr_FR/translations.php
+++ Locale/fr_FR/translations.php
@@ -1,13 +1,79 @@
 <?php
 return array(
-  'This action plugin adds a new action to the Kanboard user workflow process. Upon closing tasks, automatically email the task description and full history of comments to selected recipients.' => 'Ce plugin d\'action ajoute une nouvelle action au processus de workflow de l\'utilisateur Kanboard. À la fermeture des tâches, envoyez automatiquement par e-mail la description de la tâche et l\'historique complet des commentaires aux destinataires sélectionnés.',
-  //'Send full task description and comments by email' => 'Envoyer la description complète de la tâche et les commentaires par e-mail',
-  'EmailTaskHistory > Send task description and complete comment history by email on task closure' => 'EmailTaskHistory > Envoyer la description de la tâche et l\'historique complet des commentaires par e-mail à la fermeture de la tâche',
-  'Email subject' => 'Sujet du Courriel',
-  'Send to Assignee' => 'Envoyer au Cessionnaire',
-  'Send to Creator' => 'Envoyer au Créateur',
-  'Send to Both' => 'Envoyer aux Deux',
-  'Include Task Title and ID in subject line?' => 'Inclure le titre et l\'ID de la tâche dans la ligne d\'objet?',
-  'Task history emailed to the task creator @"%s" with subject "%s".' => 'L\'historique des tâches a été envoyé par e-mail à @"%s" (Créateur de tâches) avec pour objet "%s".',
-  'Task history emailed to the task assignee @"%s" with subject "%s".' => 'L\'historique des tâches a été envoyé par e-mail à @"%s" (Responsable de la tâche) avec le sujet "%s".',
+  //
+  // GENERAL
+  //
+  'This plugin adds a new Automatic Action to provide the user with a final email report. Upon closing tasks, automatic emails can be sent of each task description with full comment history to selected recipients or the assigned project email address.' => '',
+  'Email subject' => '',
+  //
+  // Action/EmailTaskHistory.php
+  //
+  'EmailTaskHistory > Send task description and complete comment history on task closure' => '',
+  'Send to Assignee' => '',
+  'Send to Creator' => '',
+  'Assignee & Creator' => '',
+  'Assignee & Project Email' => '',
+  'Creator & Project Email' => '',
+  'Project Email' => '',
+  'Assignee, Creator & Project Email' => '',
+  'Include the Task Title and ID in subject line?' => '',
+  'Include the Project Name in the subject line?' => '',
+  'Include the Project Identifier in the subject line?' => '',
+  'Task history emailed to the task assignee @"%s" with subject "%s".' => '',
+  'Task history emailed to the task creator @"%s" with subject "%s".' => '',
+  'Task history emailed to the project email address with subject "%s".' => '',
+  //
+  // action_creation/params.php
+  //
+  'Define action parameters' => '',
+  'Action' => '',
+  'Event' => '',
+  'Options' => '',
+  //
+  // comment/show.php
+  //
+  'Created at:' => '',
+  'Local Time' => '',
+  'LT' => '',
+  'Updated at:' => '',
+  'ID: ' => '',
+  //
+  // notification/footer.php
+  //
+  'System Generated Email' => '',
+  'View Task' => '',
+  'View Board' => '',
+  //
+  // notification/task_create.php
+  //
+  'Summary' => '',
+  'Task Status:' => '',
+  'Task N°:' => '',
+  'Project:' => '',
+  'Priority:' => '',
+  'Reference:' => '',
+  'Complexity:' => '',
+  'Created:' => '',
+  'Started:' => '',
+  'Modified:' => '',
+  'Due Date:' => '',
+  'Completed:' => '',
+  'Created by %s' => '',
+  'Assigned to %s' => '',
+  'Nobody was assigned to this task' => '',
+  'Board Column:' => '',
+  'Swimlane:' => '',
+  'Category:' => '',
+  'Task Position:' => '',
+  'Recurrence:' => '',
+  'Recurrent task is scheduled to be generated' => '',
+  'Recurrent task has been generated' => '',
+  'This task was created by: ' => '',
+  'This task has created this child task: ' => '',
+  'Description' => '',
+  //
+  // task_comments/show.php
+  //
+  'Comments' => '',
+  'No comments' => '',
 );
