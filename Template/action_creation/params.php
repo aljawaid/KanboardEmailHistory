bbf85392814030598c5e84d4c6cfacc873749e5a
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-16 22:41:15 +0000

    FIX: Remove unnecessary `label` for checkbox
    - Becomes the same word for multiple checkboxes
    - #17

--- Template/action_creation/params.php
+++ Template/action_creation/params.php
@@ -35,7 +35,6 @@
                 <?= $this->form->label($param_desc, $param_name) ?>
                 <?= $this->form->select('params['.$param_name.']', $links_list, $values) ?>
             <?php elseif ($this->text->contains($param_name, 'check_box')): ?>
-                <?= $this->form->label(t('Options'), $param_name) ?>
                 <?= $this->form->checkbox('params['.$param_name.']', $param_desc, 1, false, 'params-'.$param_name.'') ?>
             <?php elseif ($param_name === 'priority'): ?>
                 <?= $this->form->label($param_desc, $param_name) ?>
@@ -76,7 +75,6 @@
                 <?= $this->form->label($param_desc, $param_name) ?>
                 <?= $this->form->select('params['.$param_name.']', $links_list, $values) ?>
             <?php elseif ($this->text->contains($param_name, 'check_box')): ?>
-                <?= $this->form->label(t('Options'), $param_name) ?>
                 <?= $this->form->checkbox('params['.$param_name.']', $param_desc, 1, false, 'params-'.$param_name.'') ?>
             <?php elseif ($param_name === 'priority'): ?>
                 <?= $this->form->label($param_desc, $param_name) ?>
