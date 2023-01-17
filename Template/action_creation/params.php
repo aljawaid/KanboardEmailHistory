20f7ff93aef5fde584077a10ef4486fb32673b96
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-01-17 00:19:42 +0000

    FIX: HTML `<label>` Element Usage
    - #17
    - Default code is not compatible with screenreaders and keyboards as the label ID was wrong for the relevant parameter
    - #19

--- Template/action_creation/params.php
+++ Template/action_creation/params.php
@@ -21,41 +21,41 @@
             <?php foreach ($action_params as $param_name => $param_desc): ?>
                 <?php if ($this->text->contains($param_name, 'column_id')): ?>
                     <?= $this->form->label($param_desc, $param_name) ?>
-                    <?= $this->form->select('params['.$param_name.']', $columns_list, $values) ?>
+                    <?= $this->form->select($param_name, $columns_list, $values) ?>
                 <?php elseif ($this->text->contains($param_name, 'user_id')): ?>
                     <?= $this->form->label($param_desc, $param_name) ?>
-                    <?= $this->form->select('params['.$param_name.']', $users_list, $values) ?>
+                    <?= $this->form->select($param_name, $users_list, $values) ?>
                 <?php elseif ($this->text->contains($param_name, 'project_id')): ?>
                     <?= $this->form->label($param_desc, $param_name) ?>
-                    <?= $this->form->select('params['.$param_name.']', $projects_list, $values) ?>
+                    <?= $this->form->select($param_name, $projects_list, $values) ?>
                 <?php elseif ($this->text->contains($param_name, 'color_id')): ?>
-                    <?= $this->form->colorSelect('params['.$param_name.']', $values) ?>
+                    <?= $this->form->colorSelect($param_name, $values) ?>
                 <?php elseif ($this->text->contains($param_name, 'category_id')): ?>
                     <?= $this->form->label($param_desc, $param_name) ?>
-                    <?= $this->form->select('params['.$param_name.']', $categories_list, $values) ?>
+                    <?= $this->form->select($param_name, $categories_list, $values) ?>
                 <?php elseif ($this->text->contains($param_name, 'link_id')): ?>
                     <?= $this->form->label($param_desc, $param_name) ?>
-                    <?= $this->form->select('params['.$param_name.']', $links_list, $values) ?>
+                    <?= $this->form->select($param_name, $links_list, $values) ?>
                 <?php elseif ($this->text->contains($param_name, 'check_box')): ?>
                     <?= $this->form->checkbox('params['.$param_name.']', $param_desc, 1, false, 'params-'.$param_name.'') ?>
                 <?php elseif ($param_name === 'priority'): ?>
                     <?= $this->form->label($param_desc, $param_name) ?>
-                    <?= $this->form->select('params['.$param_name.']', $priorities_list, $values) ?>
+                    <?= $this->form->select($param_name, $priorities_list, $values) ?>
                 <?php elseif ($this->text->contains($param_name, 'duration')): ?>
                     <?= $this->form->label($param_desc, $param_name) ?>
-                    <?= $this->form->number('params['.$param_name.']', $values) ?>
+                    <?= $this->form->number($param_name, $values) ?>
                 <?php elseif ($this->text->contains($param_name, 'swimlane_id')): ?>
                     <?= $this->form->label($param_desc, $param_name) ?>
-                    <?= $this->form->select('params['.$param_name.']', $swimlane_list, $values) ?>
+                    <?= $this->form->select($param_name, $swimlane_list, $values) ?>
                 <?php elseif (is_array($param_desc) && ($param_name = 'send_to')): ?>
                     <?= $this->form->label(t('Email Recipient(s)'), $param_name) ?>
-                    <?= $this->form->select('params['.$param_name.']', $param_desc, $values) ?>
+                    <?= $this->form->select($param_name, $param_desc, $values) ?>
                 <?php elseif (is_array($param_desc) && (!$param_name = 'send_to')): ?>
                     <?= $this->form->label(ucfirst($param_name), $param_name) ?>
-                    <?= $this->form->select('params['.$param_name.']', $param_desc, $values) ?>
+                    <?= $this->form->select($param_name, $param_desc, $values) ?>
                 <?php else: ?>
                     <?= $this->form->label($param_desc, $param_name) ?>
-                    <?= $this->form->text('params['.$param_name.']', $values, array(), array('placeholder="'. t('Task Activity Report') .'"'), 'subject-input') ?>
+                    <?= $this->form->text($param_name, $values, array(), array('placeholder="'. t('Task Activity Report') .'"'), 'subject-input') ?>
                 <?php endif ?>
             <?php endforeach ?>
         </fieldset>
@@ -98,7 +98,7 @@
                     <?= $this->form->select('params['.$param_name.']', $param_desc, $values) ?>
                 <?php else: ?>
                     <?= $this->form->label($param_desc, $param_name) ?>
-                    <?= $this->form->text('params['.$param_name.']', $values) ?>
+                    <?= $this->form->text($param_name, $values) ?>
                 <?php endif ?>
             <?php endforeach ?>
         </fieldset>
