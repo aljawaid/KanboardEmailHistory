35a4b58af8cde06fcfe0706ef82a2a1fa1a8b161
aljawaid <10233708+aljawaid@users.noreply.github.com>
2023-05-04 21:04:19 +0100

    FIX: Code Syntax & Line Endings

--- Plugin.php
+++ Plugin.php
@@ -7,60 +7,57 @@ use Kanboard\Plugin\KanboardEmailHistory\Action\EmailTaskHistory;
 use Kanboard\Core\Translator;
 
 class Plugin extends Base
-
 {
-    
     public function initialize()
-	{
-		$this->actionManager->register(new EmailTaskHistory($this->container));
+    {
+        $this->actionManager->register(new EmailTaskHistory($this->container));
 
-		// Template Override
+        // Template Override
         //  - Override name should be camelCase e.g. pluginNameExampleCamelCase
-		$this->template->setTemplateOverride('action_creation/event', 'kanboardEmailHistory:action_creation/event');
+        $this->template->setTemplateOverride('action_creation/event', 'kanboardEmailHistory:action_creation/event');
 
-		// CSS - Asset Hook
+        // CSS - Asset Hook
         //  - Keep filename lowercase
         $this->hook->on('template:layout:css', array('template' => 'plugins/KanboardEmailHistory/Assets/css/kanboard-email-history.css'));
-	}
-	
-	public function onStartup()
-    {
-      	Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
     }
 
-	public function getPluginName()	
-	{ 		 
-		// Plugin Name MUST be identical to namespace for Plugin Directory to detect updated versions
-		return 'KanboardEmailHistory';
-	}
+    public function onStartup()
+    {
+        Translator::load($this->languageModel->getCurrentLanguage(), __DIR__ . '/Locale');
+    }
 
-	public function getPluginDescription()
-	{
-		return t('This plugin adds a new Automatic Action to provide any user or department to receive a final email report when a task is closed. Automatic emails are sent detailing the task description including the full comment history. Let this plugin create a digital file copy of your tasks using advanced options including emailing different recipients, adding comment logs and a fallback for a blank email subject line.');
-	}
+    public function getPluginName()
+    {
+        // Plugin Name MUST be identical to namespace for Plugin Directory to detect updated versions
+        return 'KanboardEmailHistory';
+    }
 
-	public function getPluginAuthor() 
-	{ 	 
-		return 'aljawaid'; 
-	}
+    public function getPluginDescription()
+    {
+        return t('This plugin adds a new Automatic Action to provide any user or department to receive a final email report when a task is closed. Automatic emails are sent detailing the task description including the full comment history. Let this plugin create a digital file copy of your tasks using advanced options including emailing different recipients, adding comment logs and a fallback for a blank email subject line.');
+    }
 
-	public function getPluginVersion() 
-	{ 	 
-		return '2.6.0';
-	}
+    public function getPluginAuthor()
+    {
+        return 'aljawaid';
+    }
+
+    public function getPluginVersion()
+    {
+        return '2.6.0';
+    }
 
     public function getCompatibleVersion()
-   	{
-       	// Examples:
-       	// >=1.0.37
+    {
+        // Examples:
+        // >=1.0.37
         // <1.0.37
-       	// <=1.0.37
+        // <=1.0.37
         return '>=1.2.20';
     }
 
-	public function getPluginHomepage() 
-	{ 	 
-		return 'https://github.com/aljawaid/KanboardEmailHistory'; 
-	}
-
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/aljawaid/KanboardEmailHistory';
+    }
 }
